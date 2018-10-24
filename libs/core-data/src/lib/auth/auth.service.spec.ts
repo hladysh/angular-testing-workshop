7043915f40b10a850708939b18d2fe09a5549638
Jonathan Garvey <jon@venturplex.com>
2018-10-24 13:11:28 -0700

    Tests for the auth service

--- libs/core-data/src/lib/auth/auth.service.spec.ts
+++ libs/core-data/src/lib/auth/auth.service.spec.ts
@@ -1,12 +1,77 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { AuthService } from './auth.service';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-// describe('AuthService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+@Injectable()
+class HttpClientStub {
+  post = jasmine.createSpy('post');
+}
 
-//   it('should be created', () => {
-//     const service: AuthService = TestBed.get(AuthService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+describe('AuthService', () => {
+  let service: AuthService;
+  let http: HttpClient;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        {provide: HttpClient, useClass: HttpClientStub}
+      ]
+    });
+
+    service = TestBed.get(AuthService);
+    http = TestBed.get(HttpClient);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('#login should post email and password', () => {
+    const data = {
+      email: 'a@b.com',
+      password: 'superdupertrupersecurer'
+    };
+
+    service.login(data.email, data.password);
+    expect(http.post).toHaveBeenCalledWith('http://localhost:3000/auth/login', data);
+  });
+
+  describe('#setToken', () => {
+    it('should set `token` value in `localStorage`', () => {
+      const token = 'kj12kj3g241hlk3j1k24j';
+      spyOn(localStorage, 'setItem');
+      service.setToken(token);
+
+      expect(localStorage.setItem).toHaveBeenCalledWith('token', token);
+    });
+
+    it('should emit truthy authentication event if token exists', () => {
+      const token = 'kj12kj3g241hlk3j1k24j';
+      spyOn(service.isAuthenticated$, 'next');
+      service.setToken(token);
+      expect(service.isAuthenticated$.next).toHaveBeenCalledWith(true);
+    });
+
+    it('should emit falsy authentication event if token is blank', () => {
+      const token = '';
+      spyOn(service.isAuthenticated$, 'next');
+      service.setToken(token);
+      expect(service.isAuthenticated$.next).toHaveBeenCalledWith(false);
+    });
+  })
+
+  it('#logout should set a blank token', () => {
+    spyOn(service, 'setToken');
+    service.logout();
+    expect(service.setToken).toHaveBeenCalledWith('');
+  });
+
+  it('#getToken should retrieve `token` from `localStorage`', () => {
+    const token = 'alsdfjasdf8967a8sdflkj';
+    localStorage.setItem('token', token);
+    expect(service.getToken()).toBe(token);
+  });
+});
