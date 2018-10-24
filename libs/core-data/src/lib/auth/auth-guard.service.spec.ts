19a92f4a493eddf44059e87383e54a63a58d7fde
Jonathan Garvey <jon@venturplex.com>
2018-10-24 12:43:11 -0700

    Tests for auth guard

--- libs/core-data/src/lib/auth/auth-guard.service.spec.ts
+++ libs/core-data/src/lib/auth/auth-guard.service.spec.ts
@@ -1,12 +1,58 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { AuthGuardService } from './auth-guard.service';
+import { AuthGuardService } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, BehaviorSubject } from 'rxjs';
 
-// describe('AuthGuardService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+@Injectable()
+class AuthServiceStub {
+  isAuthenticated$ = new BehaviorSubject(false);
+}
 
-//   it('should be created', () => {
-//     const service: AuthGuardService = TestBed.get(AuthGuardService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+@Injectable()
+class RouterStub {
+  navigate = jasmine.createSpy('navigate');
+}
+
+describe('AuthGuardService', () => {
+  let service: AuthGuardService;
+  let router: Router;
+  let authService: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuardService,
+        {provide: AuthService, useClass: AuthServiceStub},
+        {provide: Router, useClass: RouterStub}
+      ]
+    });
+
+    service = TestBed.get(AuthGuardService);
+    router = TestBed.get(Router);
+    authService = TestBed.get(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('#canActivate', () => {
+    it('should redirect user to login if not authenticated', () => {
+      service.canActivate();
+      expect(router.navigate).toHaveBeenCalledWith(['login']);
+    });
+
+    it('should return `false` if not authenticated', () => {
+      expect(service.canActivate()).toBeFalsy();
+    });
+
+    it('should return `true` if authenticated', () => {
+      authService.isAuthenticated$.next(true);
+      expect(service.canActivate()).toBeTruthy();
+    });
+  });
+
+});
